348a18631ca597985084243581c1fffa8b9f5d41
Luis Vega <vegaluisjose@gmail.com>
2019-10-19 22:47:10 -0700

    save

--- foo.rs
+++ foo.rs
@@ -1,12 +1,19 @@
 use std::fs::File;
 use std::io::{BufWriter, Write};
 
+
+// io write file
+fn emit(prog: String, path: String) {
+    let f = File::create(path).expect("Unable to create file");
+    let mut buf = BufWriter::new(f);
+    buf.write_all(prog.as_bytes()).expect("Unable to write data");
+}
+
 fn main() {
-    let mut data = String::from("Luis, ");
+    let path = String::from("foo.txt");
+    let mut data = String::from("Luis");
     data.push('\n');
     data.push_str("new line");
     println!("string: {}", data);
-    let f = File::create("foo.txt").expect("Unable to create file");
-    let mut f = BufWriter::new(f);
-    f.write_all(data.as_bytes()).expect("Unable to write data");
+    emit(data, path);
 }
