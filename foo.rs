f9a463e4ef469de8c4a4cb6f78b54994f38537d8
Luis Vega <vegaluisjose@gmail.com>
2019-10-20 13:22:56 -0700

    save

--- foo.rs
+++ foo.rs
@@ -9,32 +9,30 @@ fn write(prog: String, path: String) {
 
 #[derive(PartialEq, Hash, Debug)]
 enum Expr {
-    Module(String),
+    Clock(String),
+    Module(String, Box<Expr>),
     Main(String, Box<Expr>)
 }
 
 fn new_line() -> String { String::from("\n") }
-
-fn indent(n: usize, s: String) -> String {
-    let i = format!("{s:>w$}", s=String::from(""), w=n);
-    let is = format!("{}{}", i, s);
-    is
-}
+fn indent(n: usize) -> String { format!("{s:>w$}", s=String::from(""), w=n) }
 
 fn emit(expr: &Expr) -> String {
     use Expr::*;
     match expr {
-        Module(n) => indent(2, format!("module {}:", n)),
-        Main(n, m) => indent(0, format!("circuit {}:{}{}", n, new_line(), emit(m)))
+        Clock(n) => format!("{}input {}: Clock", indent(4), n),
+        Module(n, m) => format!("{}module {}:{}{}", indent(2), n, new_line(), emit(m)),
+        Main(n, m) => format!("{}circuit {}:{}{}", indent(0), n, new_line(), emit(m))
     }
 }
 
 fn main() {
     let s = String::from("foo");
     let t = s.clone();
-    let m = Box::new(Expr::Module(s));
-    let x = Expr::Main(t, m);
-    let prog = emit(&x);
+    let clock = Box::new(Expr::Clock(String::from("clk")));
+    let module = Box::new(Expr::Module(s, clock));
+    let top = Expr::Main(t, module);
+    let prog = emit(&top);
     let path = String::from("foo.fir");
     println!("{}", prog);
     write(prog, path);
